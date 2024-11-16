21fc48b8f2c88ae13aff3f933d75db5e8d49b48a
i am nena <nenapisannoe@yandex.ru>
2024-11-16 22:28:45 +0300

    walk & jump sound added

---
+++ Assets/Scripts/PlayerAudioPlayer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerAudioPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    [SerializeField] AudioClip[] walkSoundClips;
+
+    [SerializeField] AudioClip jumpStart;
+
+    [SerializeField] AudioClip jumpLand;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+   void PlayWalkSound()
+   {
+        int randomIndex = UnityEngine.Random.Range(0, walkSoundClips.Length);
+        audioSource.resource = walkSoundClips[randomIndex];
+
+        audioSource.Play();
+   }
+
+    void PlayJumpStart()
+    {
+        audioSource.resource = jumpStart;
+
+        audioSource.Play();
+    }
+
+    void PlayJumpLand()
+    {
+        audioSource.resource = jumpLand;
+
+        audioSource.Play();
+    }
+}
